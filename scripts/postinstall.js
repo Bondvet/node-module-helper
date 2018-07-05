9b4c0ccdebd51d107b88f2e4b1159e39336690ce
Daniel Holzmann <d@velopment.at>
2018-07-05 18:19:44 +0200

    update

--- scripts/postinstall.js
+++ scripts/postinstall.js
@@ -1,33 +1,35 @@
 const { resolve } = require('path');
 const { writeFileSync, existsSync } = require('fs');
+const addBabelConfig = require('../lib/addBabelConfig');
 
 const pwd = process.cwd();
-const pkgFile = resolve(pwd,'..', '..','..', 'package.json');
+const pkdDir = resolve(pwd, '..', '..', '..');
+const pkgFile = resolve(pkdDir, 'package.json');
 const pkgExists = existsSync(pkgFile);
 
 if (!pkgExists) {
-    console.info('no package.json found');
-    process.exit(0);
+  console.info('no package.json found');
+  process.exit(0);
 }
 
 const config = require(pkgFile);
 
-const scripts = config.scripts || {};
+const scripts = config.scripts || {};
 
 function addScript(scripts, name, cmd) {
-    const commands = [cmd];
+  const commands = [cmd];
 
-    if (scripts[name] && scripts[name] !== cmd) {
-        commands.unshift(scripts[name]);
-    }
+  if (scripts[name] && scripts[name] !== cmd) {
+    commands.unshift(scripts[name]);
+  }
 
-    scripts[name] = commands.join(' && ');
+  scripts[name] = commands.join(' && ');
 }
 
 addScript(
-    scripts,
-     'prebuild', 
-     'rm -Rf dist && mkdir -p dist && bondvet-copy-package'
+  scripts,
+  'prebuild',
+  'rm -Rf dist && mkdir -p dist && bondvet-copy-package',
 );
 
 addScript(scripts, 'build', 'babel src --out-dir dist --copy-files');
@@ -35,10 +37,12 @@ addScript(scripts, 'build:watch', 'nodemon');
 addScript(scripts, 'publish', '(cd dist && yarn publish)');
 
 config.nodemonConfig = {
-    exec: 'yarn build',
-    delay: '500',
-    watch: 'src',
+  exec: 'yarn build',
+  delay: '500',
+  watch: 'src',
 };
 config.scripts = scripts;
 
 writeFileSync(pkgFile, JSON.stringify(config, null, 2));
+
+addBabelConfig(pkdDir);
