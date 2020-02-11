260a7cf46e0438bae5d1fb856651cff56e8fa054
Daniel Holzmann <d@velopment.at>
2020-02-11 11:10:30 +0100

    use --watch for build:watch

--- scripts/postinstall.js
+++ scripts/postinstall.js
@@ -21,33 +21,28 @@ const config = require(pkgFile);
 const scripts = config.scripts || {};
 
 function addScript(scripts, name, cmd) {
-    const commands = [cmd];
-
-    if (scripts[name] && scripts[name] !== cmd) {
-        commands.unshift(scripts[name]);
+    if (!scripts[name]) {
+        scripts[name] = cmd;
     }
-
-    scripts[name] = commands.join(' && ');
 }
 
-addScript(scripts, 'prebuild', 'rm -rf build/* && mkdir -p build');
 addScript(
     scripts,
-    'postbuild',
-    'rm -rf dist/* && mkdir -p dist && bondvet-copy-package && mv build/* dist/',
+    'prebuild',
+    'rm -rf dist/* && mkdir -p dist && bondvet-copy-package',
 );
+addScript(scripts, 'prebuild:watch', 'mkdir -p dist && bondvet-copy-package');
 
-addScript(scripts, 'build', 'babel src --out-dir build --copy-files');
-addScript(scripts, 'build:watch', 'nodemon');
+addScript(scripts, 'build', 'babel src --out-dir dist --copy-files');
+addScript(
+    scripts,
+    'build:watch',
+    'babel src --out-dir dist --copy-files --watch',
+);
 addScript(scripts, 'publish', '(cd dist && yarn publish)');
 addScript(scripts, 'prepublish', 'yarn build');
 addScript(scripts, 'dev', 'yarn build:watch');
 
-config.nodemonConfig = {
-    exec: 'yarn build',
-    delay: '500',
-    watch: 'src',
-};
 config.scripts = scripts;
 
 writeFileSync(pkgFile, JSON.stringify(config, null, 2));
